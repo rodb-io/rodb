f88bb48f9f06e4ab50eeb4c35d2bf2f16b4c2618
Sébastien CAPARROS <sebastiencaparros@gmail.com>
2021-04-21 21:08:14 +0900

    Adding test for the .All() method in the XML record

--- pkg/record/xml_test.go
+++ pkg/record/xml_test.go
@@ -9,6 +9,187 @@ import (
 	"testing"
 )
 
+func TestXmlAll(t *testing.T) {
+	booleanParser := parserModule.NewBoolean(&config.BooleanParser{
+		TrueValues:  []string{"true"},
+		FalseValues: []string{"false"},
+	})
+	floatParser := parserModule.NewFloat(&config.FloatParser{
+		DecimalSeparator: ".",
+	})
+	integerParser := parserModule.NewInteger(&config.IntegerParser{})
+	jsonParser := parserModule.NewJson(&config.JsonParser{})
+	mockParser := parserModule.NewMock()
+
+	node, err := xmlquery.Parse(bytes.NewReader([]byte(`
+		<root>
+			<integer>42</integer>
+			<float>3.14</float>
+			<boolean>true</boolean>
+			<json>{"json_prop": "json val"}</json>
+			<array>
+				<item>arr a</item>
+				<item>arr b</item>
+			</array>
+			<object>
+				<prop_a>obj val a</prop_a>
+				<prop_b>obj val b</prop_b>
+			</object>
+			<arrayOfObjects>
+				<object>
+					<prop>array of obj val a</prop>
+				</object>
+				<object>
+					<prop>array of obj val b</prop>
+				</object>
+			</arrayOfObjects>
+		</root>
+	`)))
+	if err != nil {
+		t.Errorf("Unexpected error: '%v'", err)
+	}
+
+	config := &config.XmlInput{
+		Properties: []*config.XmlInputProperty{
+			{
+				Type:          config.XmlInputPropertyTypePrimitive,
+				Name:          "integer",
+				Parser:        "integer",
+				CompiledXPath: xpath.MustCompile("number(/root/integer)"),
+			},
+			{
+				Type:          config.XmlInputPropertyTypePrimitive,
+				Name:          "float",
+				Parser:        "float",
+				CompiledXPath: xpath.MustCompile("number(/root/float)"),
+			},
+			{
+				Type:          config.XmlInputPropertyTypePrimitive,
+				Name:          "boolean",
+				Parser:        "boolean",
+				CompiledXPath: xpath.MustCompile("boolean(/root/boolean[text()='true'])"),
+			},
+			{
+				Type:          config.XmlInputPropertyTypePrimitive,
+				Name:          "json",
+				Parser:        "json",
+				CompiledXPath: xpath.MustCompile("string(/root/json)"),
+			},
+			{
+				Type:          config.XmlInputPropertyTypeArray,
+				Name:          "array",
+				CompiledXPath: xpath.MustCompile("/root/array/item"),
+				Items: &config.XmlInputProperty{
+					Type:          config.XmlInputPropertyTypePrimitive,
+					CompiledXPath: xpath.MustCompile("string(/)"),
+					Parser:        "string",
+				},
+			},
+			{
+				Type:          config.XmlInputPropertyTypeObject,
+				Name:          "object",
+				CompiledXPath: xpath.MustCompile("/root/object"),
+				Properties: []*config.XmlInputProperty{
+					{
+						Type:          config.XmlInputPropertyTypePrimitive,
+						CompiledXPath: xpath.MustCompile("string(/prop_a)"),
+						Name:          "prop_a",
+						Parser:        "string",
+					},
+					{
+						Type:          config.XmlInputPropertyTypePrimitive,
+						CompiledXPath: xpath.MustCompile("string(/prop_b)"),
+						Name:          "prop_b",
+						Parser:        "string",
+					},
+				},
+			},
+			{
+				Type:          config.XmlInputPropertyTypeArray,
+				Name:          "arrayOfObjects",
+				CompiledXPath: xpath.MustCompile("/root/arrayOfObjects/object"),
+				Items: &config.XmlInputProperty{
+					Type:          config.XmlInputPropertyTypeObject,
+					CompiledXPath: xpath.MustCompile("/"),
+					Properties: []*config.XmlInputProperty{
+						{
+							Type:          config.XmlInputPropertyTypePrimitive,
+							CompiledXPath: xpath.MustCompile("string(/prop)"),
+							Name:          "prop",
+							Parser:        "string",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	parsers := parserModule.List{
+		"string":  mockParser,
+		"json":    jsonParser,
+		"boolean": booleanParser,
+		"float":   floatParser,
+		"integer": integerParser,
+	}
+
+	record, err := NewXml(config, node, parsers, 0)
+	if err != nil {
+		t.Errorf("Unexpected error: '%v'", err)
+	}
+
+	result, err := record.All()
+	if err != nil {
+		t.Errorf("Unexpected error: '%v'", err)
+	}
+
+	if expect, got := 42, result["integer"].(int); got != expect {
+		t.Errorf("Expected to get '%v', got '%v'", expect, got)
+	}
+	if expect, got := 3.14, result["float"].(float64); got != expect {
+		t.Errorf("Expected to get '%v', got '%v'", expect, got)
+	}
+	if expect, got := true, result["boolean"].(bool); got != expect {
+		t.Errorf("Expected to get '%v', got '%v'", expect, got)
+	}
+
+	json := result["json"].(map[string]interface{})
+	if expect, got := "json val", json["json_prop"].(string); got != expect {
+		t.Errorf("Expected to get '%v', got '%v'", expect, got)
+	}
+
+	array := result["array"].([]interface{})
+	if expect, got := 2, len(array); got != expect {
+		t.Errorf("Expected a length of '%v', got '%v'", expect, got)
+	}
+	if expect, got := "arr a", array[0].(string); got != expect {
+		t.Errorf("Expected to get '%v', got '%v'", expect, got)
+	}
+	if expect, got := "arr b", array[1].(string); got != expect {
+		t.Errorf("Expected to get '%v', got '%v'", expect, got)
+	}
+
+	object := result["object"].(map[string]interface{})
+	if expect, got := "obj val a", object["prop_a"].(string); got != expect {
+		t.Errorf("Expected to get '%v', got '%v'", expect, got)
+	}
+	if expect, got := "obj val b", object["prop_b"].(string); got != expect {
+		t.Errorf("Expected to get '%v', got '%v'", expect, got)
+	}
+
+	arrayOfObjects := result["arrayOfObjects"].([]interface{})
+	if expect, got := 2, len(arrayOfObjects); got != expect {
+		t.Errorf("Expected a length of '%v', got '%v'", expect, got)
+	}
+	arrayOfObjects0 := arrayOfObjects[0].(map[string]interface{})
+	if expect, got := "array of obj val a", arrayOfObjects0["prop"].(string); got != expect {
+		t.Errorf("Expected to get '%v', got '%v'", expect, got)
+	}
+	arrayOfObjects1 := arrayOfObjects[1].(map[string]interface{})
+	if expect, got := "array of obj val b", arrayOfObjects1["prop"].(string); got != expect {
+		t.Errorf("Expected to get '%v', got '%v'", expect, got)
+	}
+}
+
 func TestXmlGet(t *testing.T) {
 	booleanParser := parserModule.NewBoolean(&config.BooleanParser{
 		TrueValues:  []string{"true"},
