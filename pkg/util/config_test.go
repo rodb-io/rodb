f95041d69766047e24c70de154e185017ad734b4
Sébastien CAPARROS <sebastiencaparros@gmail.com>
2021-05-05 18:15:55 +0900

    Testing util.GetTypeFromConfigUnmarshaler

---
+++ pkg/util/config_test.go
@@ -0,0 +1,57 @@
+package util
+
+import (
+	"testing"
+)
+
+func TestGetTypeFromConfigUnmarshaler(t *testing.T) {
+	t.Run("normal", func(t *testing.T) {
+		unmarshal := func(data interface{}) error {
+			dataMap := data.(map[string]interface{})
+			dataMap["type"] = "test"
+			return nil
+		}
+
+		got, err := GetTypeFromConfigUnmarshaler(unmarshal)
+		if err != nil {
+			t.Errorf("Unexpected error: '%v'", err)
+		}
+		if expect := "test"; got != expect {
+			t.Errorf("Expected to get '%v', got '%v'", expect, got)
+		}
+	})
+	t.Run("missing type", func(t *testing.T) {
+		unmarshal := func(data interface{}) error {
+			return nil
+		}
+
+		_, err := GetTypeFromConfigUnmarshaler(unmarshal)
+		if err == nil {
+			t.Errorf("Expected error, got '%v'", err)
+		}
+	})
+	t.Run("wrong type", func(t *testing.T) {
+		unmarshal := func(data interface{}) error {
+			dataMap := data.(map[string]interface{})
+			dataMap["type"] = 42
+			return nil
+		}
+
+		_, err := GetTypeFromConfigUnmarshaler(unmarshal)
+		if err == nil {
+			t.Errorf("Expected error, got '%v'", err)
+		}
+	})
+	t.Run("empty", func(t *testing.T) {
+		unmarshal := func(data interface{}) error {
+			dataMap := data.(map[string]interface{})
+			dataMap["type"] = ""
+			return nil
+		}
+
+		_, err := GetTypeFromConfigUnmarshaler(unmarshal)
+		if err == nil {
+			t.Errorf("Expected error, got '%v'", err)
+		}
+	})
+}
