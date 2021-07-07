df195013b83ad965e066cf5663780cde0fc4554c
Sébastien CAPARROS <sebastiencaparros@gmail.com>
2021-07-07 21:18:29 +0900

    Revert "Moving the parser type list out of the config package"
    This reverts commit ee8bd02364ac2d5b9824b81b908f78d7ad4819da.

--- pkg/config/parser.go
+++ pkg/config/parser.go
@@ -16,10 +16,27 @@ func (config *parserParser) UnmarshalYAML(unmarshal func(interface{}) error) err
 		return fmt.Errorf("Error in parser config: %w", err)
 	}
 
-	config.parser, err = parser.NewConfigFromType(objectType)
-	if err != nil {
-		return fmt.Errorf("Error in parser config: %w", err)
+	switch objectType {
+	case "integer":
+		config.parser = &parser.IntegerConfig{}
+		return unmarshal(config.parser)
+	case "float":
+		config.parser = &parser.FloatConfig{}
+		return unmarshal(config.parser)
+	case "boolean":
+		config.parser = &parser.BooleanConfig{}
+		return unmarshal(config.parser)
+	case "string":
+		config.parser = &parser.StringConfig{}
+		return unmarshal(config.parser)
+	case "json":
+		config.parser = &parser.JsonConfig{}
+		return unmarshal(config.parser)
+	case "split":
+		config.parser = &parser.SplitConfig{}
+		return unmarshal(config.parser)
+	default:
+		return fmt.Errorf("Error in parser config: Unknown type '%v'", objectType)
 	}
-
-	return unmarshal(config.parser)
 }
+
